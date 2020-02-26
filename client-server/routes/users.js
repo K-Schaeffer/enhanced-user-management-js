fb1db97b1732f7a50ee27d4cfb8cf963adc482d8
K-Schaeffer <kcb.schaeffer@gmail.com>
2020-02-26 20:00:12 -0300

    Consuming routes - Post, Put, etc... With Restify

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -15,7 +15,55 @@ router.get('/', function (req, res, next) { //Express params
   client.get('/users', function (err, request, response, obj) { //Restify params
     assert.ifError(err);
 
-    res.end(JSON.stringify(obj, null, 2));
+    res.json(obj);
+
+  });
+
+});
+
+/* GET specific user. */
+router.get('/:id', function (req, res, next) { //Express params
+
+  client.get(`/users/${req.params.id}`, function (err, request, response, obj) {
+    assert.ifError(err);
+
+    res.json(obj);
+
+  });
+
+});
+
+/* Edit user  */
+router.put('/:id', function (req, res, next) { //Express params
+
+  client.put(`/users/${req.params.id}`, req.body, function (err, request, response, obj) {
+    assert.ifError(err);
+
+    res.json(obj);
+
+  });
+
+});
+
+/* Delete user  */
+router.delete('/:id', function (req, res, next) { //Express params
+
+  client.del(`/users/${req.params.id}`, function (err, request, response, obj) { // del is the delete from restify
+    assert.ifError(err);
+
+    res.json(obj);
+
+  });
+
+});
+
+/* Create user  */
+router.post('/', function (req, res, next) { //Express params
+
+  client.post(`/users/`, req.body, function (err, request, response, obj) { // del is the delete from restify
+    assert.ifError(err);
+
+    res.json(obj);
 
   });
 
